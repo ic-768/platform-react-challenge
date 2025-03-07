5bc43e38c001669c9a032959aa7852f9c077244b
ic768 <ic768@protonmail.com>
2025-03-07 16:49:39 +0200

    minor css

--- src/components/ui/favorite-button.tsx
+++ src/components/ui/favorite-button.tsx
@@ -12,7 +12,7 @@ export default function FavoriteButton({
 
   return (
     <button
-      className="sticky bottom-12 flex size-14 cursor-pointer items-center justify-center rounded-full bg-red-100 outline outline-red-500/60 transition-colors hover:bg-red-200"
+      className="sticky bottom-16 flex size-14 cursor-pointer items-center justify-center self-end rounded-full bg-red-100 outline outline-red-500/60 transition-colors hover:bg-red-200"
       onClick={onClick}
     >
       <Heart size={32} {...iconProps} />
